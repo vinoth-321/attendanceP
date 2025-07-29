ccd8f13e4026c88567f0c261ead51e02825e9783
vinoth-321 <vinothkumar15122005@gmail.com>
2025-07-29 15:01:12 +0530

    new

--- attendanceP/settings.py
+++ attendanceP/settings.py
@@ -77,14 +77,21 @@ WSGI_APPLICATION = 'attendanceP.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600,
-        ssl_require=False  # Set to True only if you're using PostgreSQL on Render
-    )
-}
-
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
