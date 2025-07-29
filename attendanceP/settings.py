e71a2dc14af716342a2f697d4ce004cb9b77797b
vinoth-321 <vinothkumar15122005@gmail.com>
2025-07-29 14:43:14 +0530

    new

--- attendanceP/settings.py
+++ attendanceP/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +78,11 @@ WSGI_APPLICATION = 'attendanceP.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': dj_database_url.config(
+        default=os.environ.get('postgresql://spoko_user:JLfzGw8m3oHuc0rHG7pooi1x93WOkbGH@dpg-d248rbh5pdvs738262eg-a/spoko_cka8'),
+        conn_max_age=600,  # for performance
+        ssl_require=True   # required by Render
+    )
 }
 
 
